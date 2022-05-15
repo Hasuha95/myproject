45adf6fe412a27632b16734fe526c204afb94503
Hasuha <xacyxanovich@mail.ru>
2022-05-15 13:14:48 +0300

    структура проектов выстроена верно

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("my first github project");
-    }
-    public static void branch1() {
-        System.out.println("branch1 complete");
-    }
-}
---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,31 @@
+package lesson1;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner riad = new Scanner(System.in);
+        System.out.println("print a=");
+        float a = riad.nextInt();
+        System.out.println("print b=");
+        float b = riad.nextInt();
+        System.out.println("print c=");
+        float c = riad.nextInt();
+        System.out.println("print d=");
+        float d = riad.nextInt();
+
+
+        float calc = a*(b+(c/d));
+        System.out.println("calculation");
+
+
+
+
+
+        System.out.println("result="+calc);
+    }
+
+
+
+}
