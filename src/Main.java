cfd03661b8f38cca8a8e1314594758962d9280f8
Hasuha <xacyxanovich@mail.ru>
2022-05-15 12:12:50 +0300

    commited branch1

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("my first github project");
     }
+    public static void branch1() {
+        System.out.println("branch1 complete");
+    }
 }
